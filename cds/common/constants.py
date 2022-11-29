4ce76305e2937ab21b6d9f5e485792fce28208a4
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2022-11-29 19:03:56 +0000

    ✨ add constant for generator list

--- cds/common/constants.py
+++ cds/common/constants.py
@@ -1,3 +1,5 @@
 p30_seq_files_bucket_name = "cds-246-phs002517-sequencefiles-p30-fy20"
 
-submission_package_default_dir = "data/submission_packet"
+submission_package_default_dir = "data/submission_packet/"
+
+all_generator_list = ["all", "participant", "sample", "file", "genomic_info"]
