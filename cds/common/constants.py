b2d195fdbe6e8e8118e6fd0022bfc404462c9330
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2022-12-01 13:30:13 +0000

    ✨ add dummy postgres connection url

--- cds/common/constants.py
+++ cds/common/constants.py
@@ -2,4 +2,15 @@ p30_seq_files_bucket_name = "cds-246-phs002517-sequencefiles-p30-fy20"
 
 submission_package_default_dir = "data/submission_packet/"
 
-all_generator_list = ["all", "participant", "sample", "file", "genomic_info"]
+all_generator_list = [
+    "all",
+    "participant",
+    "sample",
+    "diagnosis",
+    "file",
+    "genomic_info",
+]
+
+default_postgres_url = (
+    "postgresql://username:password@localhost:5432/default_database"
+)
