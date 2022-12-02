3a00aa1921a25886ae7a4c62d80936a489caddd2
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2022-12-02 15:08:10 +0000

    ✨ add file generator

--- cds/generator/file/build_manifest.py
+++ cds/generator/file/build_manifest.py
@@ -2,7 +2,8 @@ from ast import literal_eval
 
 from d3b_cavatica_tools.utils.logging import get_logger
 
-from cds.common.queries import file_query
+from cds.common.constants import seq_file_bucket_name
+from cds.common.queries import file_bucket_query, file_kf_query, file_query
 
 import pandas as pd
 import psycopg2
@@ -10,11 +11,12 @@ import psycopg2
 logger = get_logger(__name__, testing_mode=False)
 
 
-def build_file_table(db_url, file_list, submission_packager_dir):
+def build_file_table(db_url, file_list, submission_package_dir):
     logger.info("Building file table")
     logger.info("connecting to database")
     conn = psycopg2.connect(db_url)
 
     logger.info("Querying for manifest of files")
-    file_info = pd.read_sql(file_query(file_list), conn)
-    breakpoint()
+    file_table = pd.read_sql(file_query(file_list, seq_file_bucket_name), conn)
+    logger.info("saving file manifest to file")
+    file_table.to_csv(f"{submission_package_dir}/file.csv", index=False)
