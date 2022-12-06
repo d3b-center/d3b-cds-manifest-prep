cf4d7b99e9f514caec98d65817274540664c358a
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2022-12-06 19:17:05 +0000

    🐛 didn't uncomment the important parts

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -5,10 +5,10 @@ mkdir -p tmp
 
 # Add new version to bug report form
 awk -v version_id="${1}" '/new_versions_here/ { print; print "        - \x22" version_id "\x22"; next }1' .github/ISSUE_TEMPLATE/submission_pkg_bug_report.yml > tmp/submission_bug_report
-# cp tmp/submission_bug_report .github/ISSUE_TEMPLATE/submission_pkg_bug_report.yml
+cp tmp/submission_bug_report .github/ISSUE_TEMPLATE/submission_pkg_bug_report.yml
 
 # Update the readme
 awk -v version_id="${1}" '/insert_version_url_here/ { print; print "The most recently generated manifest can be found [here](https://github.com/d3b-center/d3b-cds-manifest-prep/releases/tag/" version_id ")."; next }1' README.md > tmp/readme
-# cp tmp/readme README.md
+cp tmp/readme README.md
 
 rm -rf tmp
