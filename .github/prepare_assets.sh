8c2e765bf49034ba848ec4eb521a928a7d139068
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2022-12-06 14:04:43 +0000

    🚸 output file in zip now has version name and bundles changelog

--- .github/prepare_assets.sh
+++ .github/prepare_assets.sh
@@ -3,6 +3,13 @@ set -e
 
 ZIP_NAME="data/zip/submission_package_${1}.zip"
 
-zip -r "${ZIP_NAME}"  data/submission_packet/*
+ZIP_DATA_DIR="submission_package_v${1}"
+
+mkdir "${ZIP_DATA_DIR}"
+
+cp -r data/submission_packet/ "${ZIP_DATA_DIR}" 
+cp CHANGELOG.md "${ZIP_DATA_DIR}"
+
+zip -r "${ZIP_NAME}" "${ZIP_DATA_DIR}/" -x "${ZIP_DATA_DIR}/submission_packet/README.md"
 
 echo "${ZIP_NAME}" > .github/release_assets.txt
