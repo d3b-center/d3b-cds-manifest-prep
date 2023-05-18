1c993f5baf4fe24776134dbc38ed5c4bcf562399
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2023-05-18 14:36:39 -0400

    ✨ generate the study file

--- cds/generator/study/study.py
+++ cds/generator/study/study.py
@@ -1,6 +1,7 @@
 from d3b_cavatica_tools.utils.logging import get_logger
 
 from cds.common.columns import order_columns
+from cds.common.tables import OutputTable
 
 import pandas as pd
 import psycopg2
@@ -8,29 +9,23 @@ import psycopg2
 logger = get_logger(__name__, testing_mode=False)
 
 
-def build_study_table(
-    db_url, participant_list, submission_package_dir, submission_template_dict
-):
-    """Build the participant table
+def build_study_table(submission_package_dir, submission_template_dict):
+    """Build the study table
 
-    Build the participant manifest
+    Build the study manifest
 
-    :param db_url: database url to KF prd postgres
-    :type db_url: str
-    :param participant_list: participant IDs to have in the participant manifest
-    :type participant_list: list
     :param submission_package_dir: directory to save the output manifest
     :type submission_package_dir: str
-    :return: participant table
-    :rtype: pandas.DataFrame
+    :param submission_template_dict: template submission manifests
+    :type submission_template_dict: dict
+    :return: study table
+    :rtype: OutputTable
     """
-    output_table = submission_template_dict["study"]
-    # Set the column order and sort on key column
-    output_table = order_columns(
-        output_table, "study", submission_template_dict
-    ).sort_values("participant_id")
-    logger.info("saving sample manifest to file")
-    output_table.to_csv(
-        f"{submission_package_dir}/participant.csv", index=False
+    table_object = OutputTable(
+        "study", submission_template_dict=submission_template_dict
     )
-    return output_table
+    # Build the output table
+    table_object.template_is_output()
+    # Save the table
+    table_object.save_table(submission_package_dir)
+    return table_object
