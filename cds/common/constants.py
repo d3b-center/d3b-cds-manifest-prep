7a582536d31185c344d6cc288f67f3720e63f2d4
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2023-05-22 13:40:27 +0000

    🍱 default submission package shouldn't have trailing slask

--- cds/common/constants.py
+++ cds/common/constants.py
@@ -2,7 +2,7 @@ import pkg_resources
 
 p30_seq_files_bucket_name = "cds-246-phs002517-sequencefiles-p30-fy20"
 
-submission_package_default_dir = "data/submission_packet/"
+submission_package_default_dir = "data/submission_packet"
 
 
 all_generator_list = {
